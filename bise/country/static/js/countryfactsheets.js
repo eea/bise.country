a8ab15ab5672dd11e40c92c06d56626a1bfa86ff
Krisztina Elekes <krisztina.n.elekes@gmail.com>
2017-09-22 13:01:07 +0300

    Refs #85826; Sidebar menu for contributions

--- bise/country/static/js/countryfactsheets.js
+++ bise/country/static/js/countryfactsheets.js
@@ -80,4 +80,111 @@ $(document).ready(function(){
     }
   });
 
+
+  // contributions sidebar
+
+  $('.anchor-link').hide();
+  var $smwrap = $('<div id="mtr-wrapper"/>');
+  var $srow = $('<div id="srow" class="row"/>');
+  var $swrap = $('<div id="mtr-main-wrapper" class="tab-content col-md-9 pull-right"/>');
+  var $sside = $('<div id="sidebar-wrapper" class="col-md-3 sidebar">sidebar</div>')
+
+  $('.country-table').wrapAll($smwrap);
+  $('.country-table').wrapAll($srow);
+  $('.country-table').wrapAll($swrap);
+
+  var $wrapper = $('#srow');
+
+
+  var _gtarc = 0;    // global counter for name targets
+
+  $(function(){
+    var $sidebar = $('#sidebar-wrapper');
+    var sections = [];
+
+    $('tr').each(function(){
+      var $tr = $(this);
+      var section_title = $('h2', this).text();
+      var cl = $tr.attr('class');
+
+      if (cl && cl.indexOf('target') !== -1) {
+        var section_descr = $('td:nth-child(2) p', this).text();
+      }
+
+      if (section_title) {
+        sections.push([section_title, section_descr, []]);
+      }
+
+      $('td', this).each(function() {
+        var $td = $(this);
+        var klass = $td.attr('class');
+        var text = $td.text().trim();
+
+        if (text.indexOf('Action') === 0) {
+          _gtarc += 1;
+          $td.attr('id', 'gtarc-' + _gtarc);
+          sections[sections.length - 1][2].push([text, [_gtarc]]);
+        }
+
+      })
+
+    });
+
+    var $ssidebar = $('<div id="sidebar-wrapper" class="col-md-3 sidebar"/>');
+    var $menu = $('<ul class="nav nav-list nav-menu-list-style"/>');
+
+    $ssidebar.append($menu);
+    $wrapper.append($ssidebar);
+
+    for (var i = 0; i < sections.length; i++) {
+
+      var sectionTitle= sections[i][0].toString().slice(3);
+      var sectionTitle = sectionTitle.charAt(0).toUpperCase() + sectionTitle.slice(1).toLowerCase();
+      var sectionDescr = sections[i][1];
+      var targetID = sectionTitle.slice(-1);
+      var $sa = $('<a/>').attr('href', '#eu-target-' + targetID);
+      $sa.text(sectionTitle);
+      var $sp = $('<p/>');
+      $sp.text(sectionDescr);
+      var $ss = $('<span class="tree-toggle nav-header"/>');
+      $ss.append($sa);
+      $ss.append($sp);
+      var $sli = $('<li/>');
+      $sli.append($ss);
+      $menu.append($sli);
+      var sectionActions = sections[i][2];
+      var $sul = $('<ul class="nav nav-list trees bullets"/>');
+      $sli.append($sul);
+
+      for (var j = 0; j < sectionActions.length; j++) {
+        var action = sectionActions[j][0];
+        var subText = action.split(':');
+        var subTitle = subText[0];
+        var subDescription = subText[1];
+        var actionID = sectionActions[j][1];
+        var navActionTitle = $('<a class="action-title"/>').attr('href','#gtarc-' + actionID).text(subTitle);
+        var navActionDescription = $('<p class="action-description" />').text(subDescription);
+        var $sp = $('<span/>').append(navActionTitle, navActionDescription);
+        var $ali = $('<li/>');
+        $ali.append($sp);
+        $sul.append($ali);
+      }
+    }
+
+    $('.tree-toggle').click(function () {
+      $(this).parent().children('ul.trees').toggle(200);
+    });
+
+    $(function() {
+      $('.tree-toggle').parent().children('ul.trees').toggle(200);
+    });
+
+    $('.sidebar li ul li span').click(function(e) {
+      $('.sidebar li ul li span').removeClass('selected');
+      $(this).addClass('selected');
+      return true;
+    });
+
+  })
+
 });
