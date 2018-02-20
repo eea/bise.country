94cd40d91d081a7ddddb2af23e5ac0eb7387f6f3
Krisztina Elekes <krisztina.n.elekes@gmail.com>
2018-02-20 10:32:08 +0200

    Refs #90236; Improve country profile overview

--- bise/country/browser/views.py
+++ bise/country/browser/views.py
@@ -2,7 +2,9 @@ import json
 
 from lxml.builder import E
 from lxml.html import fromstring, tostring
-
+from plone.app.contentlisting.interfaces import (IContentListing,
+                                                 IContentListingObject)
+from Acquisition import aq_inner
 from plone.api.user import has_permission
 from plone.subrequest import subrequest
 
@@ -74,6 +76,43 @@ class MapSingleCountrySettings(object):
 class CountryFactsheetView(object):
     """
     """
+    def facts(self):
+        context = aq_inner(self.context)
+        sections = context.getFolderContents({'portal_type': 'Section'})
+        fact_data = []
+
+        targets = self.request.form.get('targets', '')
+
+        for section in sections:
+            data = {}
+            data['object'] = IContentListingObject(section)
+            section_object = data['object'].getObject()
+            facts = section_object.getFolderContents({'portal_type': 'Fact'})
+            fact_list = []
+
+            if targets == "":
+                fact_list = facts
+                data['facts'] = IContentListing(fact_list)
+                fact_data.append(data)
+            else:
+                if section_object.Title() == "General information":
+                    fact_list = facts
+                    data['facts'] = IContentListing(fact_list)
+                    fact_data.append(data)
+                else:
+                    for fact in facts:
+                        targetsArray = targets.split(",")
+
+                        if fact.getObject().targets is not None:
+                            for target in targetsArray:
+                                if target in fact.getObject().targets:
+                                    fact_list.append(fact)
+
+                    if len(fact_list) > 0:
+                        data['facts'] = IContentListing(fact_list)
+                        fact_data.append(data)
+
+        return fact_data
 
     def can_edit(self, obj):
         return has_permission('Modify portal content', obj=obj)
