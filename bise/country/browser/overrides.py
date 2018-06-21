1344cfee7bc8b9e9f5b8c575b0749bdfce7d3284
Petchesi-Iulian <iamnotacat12@gmail.com>
2018-06-21 15:56:24 +0300

    Refs #95356 Hide the add items menu from contributors on country factsheets.

--- bise/country/browser/overrides.py
+++ bise/country/browser/overrides.py
@@ -1,6 +1,15 @@
 from plone.api.user import get_roles
 from plone.app.contentmenu.menu import ActionsSubMenuItem as ASMI
+from plone.app.contentmenu.menu import FactoriesSubMenuItem as FSMI
 from plone.app.layout.globals.context import ContextState as CS
+
+
+class FactoriesSubMenuItem(FSMI):
+
+    def available(self):
+        local_roles = get_roles(obj=self.context, inherit=True)
+        # hide Factories menu for Contributor
+        return 'Contributor' not in local_roles
 
 
 class ActionsSubMenuItem(ASMI):
